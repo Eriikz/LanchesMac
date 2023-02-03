f8064b98a72e54ba4867f3223c859a9b0196d5a8
Erik Fernandess <erik.fernandess@ibm.com>
2023-02-02 21:31:06 -0300

    lanche

---
+++ LanchesMac/LanchesMac/Controllers/ContatoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+          
+        }
+    }
+}
